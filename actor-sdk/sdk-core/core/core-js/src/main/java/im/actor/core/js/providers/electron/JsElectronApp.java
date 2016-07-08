bc66e82db50bb8fa1b82765347cecd946ba66c98
rdc-justep <song@justep.com>
2016-07-08 06:18:37 +0000

    支持electron app
    git-svn-id: http://192.168.1.20/svn/im/trunk/ActorSdk@226 0c17fa83-6788-ff4f-83ba-5dfcf0814e92

--- actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/electron/JsElectronApp.java
+++ actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/electron/JsElectronApp.java
@@ -3,34 +3,34 @@ package im.actor.core.js.providers.electron;
 public class JsElectronApp {
 
     public static native boolean isElectron()/*-{
-        return  (!'requirejs' in $wnd) && ('require' in $wnd);
+        return  navigator.userAgent.indexOf("Electron") !=-1;
     }-*/;
 
     public static native void bounce()/*-{
-        var ipc = $wnd.require("electron").ipcRenderer;
+        var ipc = $wnd.requireNode("electron").ipcRenderer;
         ipc.send('tray-bounce');
     }-*/;
 
     public static native void hideNewMessages()/*-{
-        var ipc = $wnd.require("electron").ipcRenderer;
+        var ipc = $wnd.requireNode("electron").ipcRenderer;
         ipc.send('new-messages-hide');
     }-*/;
 
     public static native void updateBadge(int count)/*-{
-        var ipc = $wnd.require("electron").ipcRenderer;
+        var ipc = $wnd.requireNode("electron").ipcRenderer;
         ipc.send('tray-badge', { count: count });
     }-*/;
 
 
     public static native void subscribe(String topic, JsElectronListener listener)/*-{
-        var ipc = $wnd.require("electron").ipcRenderer;
+        var ipc = $wnd.requireNode("electron").ipcRenderer;
         ipc.on(topic, function(message) {
             listener.@im.actor.core.js.providers.electron.JsElectronListener::onEvent(*)(message);
         });
     }-*/;
 
     public static native void openUrlExternal(String url)/*-{
-        var shell = $wnd.require('electron').shell;
+        var shell = $wnd.requireNode('electron').shell;
         shell.openExternal(url);
     }-*/;
 }
